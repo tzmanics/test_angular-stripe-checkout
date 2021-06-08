22353bcaa52084d815eac5a4d434e0cc49c85e7a
Tara Z. Manicsic <tzmanics@gmail.com>
2021-06-07 23:48:20 -0400

    changes name

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -6,7 +6,7 @@ describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [RouterTestingModule],
-      declarations: [AppComponent]
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -16,10 +16,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-universal-pre-render'`, () => {
+  it(`should have as title 'angular-stripe-checkout'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-universal-pre-render');
+    expect(app.title).toEqual('angular-stripe-checkout');
   });
 
   it('should render title', () => {
@@ -27,7 +27,7 @@ describe('AppComponent', () => {
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
     expect(compiled.querySelector('.content span').textContent).toContain(
-      'angular-universal-pre-render app is running!'
+      'angular-stripe-checkout app is running!'
     );
   });
 });
