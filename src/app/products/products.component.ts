00f6f5eac34aae8bf82d573485e5b332c68f1843
Tara Z. Manicsic <tzmanics@gmail.com>
2021-06-09 23:33:43 -0400

    imports json products + starts checkout code

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,12 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { loadStripe } from '@stripe/stripe-js';
+import products from '../products.json';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.css']
+  styleUrls: ['./products.component.css'],
 })
 export class ProductsComponent implements OnInit {
-  constructor() {}
+  products: any = products;
+  private response: any;
+
+  constructor(private http: HttpClient) {}
+
+  async triggerCreateCheckout(eventProduct: any) {
+    this.response = await this.http
+      .post('/.netlify/functions/createCheckout', eventProduct, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+      .toPromise();
+    this.openStripe(this.response);
+  }
+
+  openStripe = async (stripeParams: any) => {
+    const stripe = await loadStripe(stripeParams.publishableKey);
+    const { error } = await stripe!.redirectToCheckout({
+      sessionId: stripeParams.sessionId,
+    });
+  };
 
   ngOnInit(): void {}
 }
